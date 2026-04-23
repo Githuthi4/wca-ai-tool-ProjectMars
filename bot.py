a237fba9b60aa3e31ae381c1db33e21767c1212c
joacqiummoriasi-design <joacqiummoriasi@gmail.com>
2026-04-23 18:09:03 +0300

    Correct API call syntax in bot.py
    Fix API call parameters for generating quiz questions.

--- bot.py
+++ bot.py
@@ -22,5 +22,16 @@ def run_quiz():
             break
         
         print("Generating questions...")
+        try:
+            # 2. THE API CALL 
+            #1.5-flash is the 'safe' choice for free users
+            responce = client.models.generate_content(
+                model="gemine-2.5-flash",
+                f"Task:create a five 5-question multiple choice quiz on:{user_topics}",
+               " context: The quiz is for a students' self assesment"
+                "constrains return only JSON"
+                "output:{'quiz:[{'question':'...','options':['A)..','B) ..'],'correct_answer':'A'}]}"
+             )
+            )
 
         
