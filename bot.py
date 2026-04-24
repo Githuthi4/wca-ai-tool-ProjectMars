3b513abbe301669655fbbd7a98decc43721da8ce
Githuthi4 <githuthimary@gmail.com>
2026-04-24 10:39:08 +0300

    Corrected spelling mistakes

--- bot.py
+++ bot.py
@@ -23,14 +23,15 @@ def run_quiz():
         
         print("Generating questions...")
         try:
-            # 2. THE API CALL 
-            #1.5-flash is the 'safe' choice for free users
-            responce = client.models.generate_content(
-                model="gemine-2.5-flash",
-                f"Task:create a five 5-question multiple choice quiz on:{user_topics}",
-               " context: The quiz is for a students' self assesment"
-                "constrains return only JSON"
-                "output:{'quiz:[{'question':'...','options':['A)..','B) ..'],'correct_answer':'A'}]}"
+        
+            # RTCCO format
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                contents= (
+                f"Task: Create a five 5-question multiple choice quiz on:{user_topic}."
+                "Context: The quiz is for a students' self assesment"
+                "Constraint: Return only JSON"
+                "Output:{'quiz:[{'question':'...','options':['A)..','B) ..'],'correct_answer':'A'}]}"
              )
             )
 
